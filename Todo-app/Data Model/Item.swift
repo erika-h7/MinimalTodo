e204f74474e5f937ecfc152b24af3a9691e5543e
erika-h7 <erika.herrera1@outlook.com>
2022-10-14 12:02:54 -0700

    Created a Custom Data Model.

---
+++ Todo-app/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todo-app
+//
+//  Created by Infinity Code on 10/13/22.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
