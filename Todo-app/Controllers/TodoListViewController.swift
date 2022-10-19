d8308c468a810a6e0803d86f56df78bf1b184058
erika-h7 <erika.herrera1@outlook.com>
2022-10-19 10:51:08 -0700

    Updated Realm Data Base by using real.write method.

--- Todo-app/Controllers/TodoListViewController.swift
+++ Todo-app/Controllers/TodoListViewController.swift
@@ -55,6 +55,18 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+            }
+            
+            } catch {
+                print("Error saving done status, \(error)")
+            }
+        }
+        
+        tableView.reloadData()
         // toggle done check mark
         
         //        context.delete(itemArray[indexPath.row])
