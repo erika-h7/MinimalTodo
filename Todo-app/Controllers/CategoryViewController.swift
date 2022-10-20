6bbd77e1d2a2a2e9f8368afd2be984953e3debed
erika-h7 <erika.herrera1@outlook.com>
2022-10-20 12:14:27 -0700

    Made Our Cells Swipeable.

--- Todo-app/Controllers/CategoryViewController.swift
+++ Todo-app/Controllers/CategoryViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
+
 
 class CategoryViewController: UITableViewController {
     
@@ -19,6 +21,8 @@ class CategoryViewController: UITableViewController {
         super.viewDidLoad()
 
         loadCategories()
+        
+        tableView.rowHeight = 75.0
     }
 
     
@@ -30,11 +34,21 @@ class CategoryViewController: UITableViewController {
         
     }
     
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
+//        cell.delegate = self
+//        return cell
+//    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "🚧 NO CATEGORIES ADDED ☝️"
+        
+        cell.delegate = self
         
         print("This is the cell >>>>>>>>> \(cell)")
         
@@ -111,3 +125,69 @@ class CategoryViewController: UITableViewController {
     }
 }
 
+
+    // MARK: - Swipe Cell Delegate Methods
+
+extension CategoryViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+
+        let deleteAction = SwipeAction(style: .destructive, title: nil) { action, indexPath in
+            print("Item deleted")
+            
+            if let categoryForDeletion = self.categories?[indexPath.row] {
+                do {
+                    try self.realm.write {
+                        self.realm.delete(categoryForDeletion)
+                    }
+                } catch {
+                    print("Error deleting category, \(error)")
+                }
+                
+                tableView.reloadData()
+            }
+        }
+
+        // customize the action appearance
+        deleteAction.transitionDelegate = ScaleTransition.default
+        deleteAction.textColor = UIColor(.red)
+        deleteAction.backgroundColor = UIColor(.white)
+       
+        let imageIcon = self.resizeImage(image: UIImage(named: "trash")!, targetSize: CGSizeMake(40.0, 40.0))
+//        let imageIcon = UIImage(named: "trash")
+        deleteAction.image = imageIcon
+        
+        
+        
+
+        return [deleteAction]
+    }
+    
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+}
